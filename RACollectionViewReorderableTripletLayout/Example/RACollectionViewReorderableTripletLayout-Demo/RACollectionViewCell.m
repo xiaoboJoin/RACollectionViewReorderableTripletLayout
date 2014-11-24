82eea17a7aaad046b7c0816a24ece41481a86858
xiaoboJoin <xiaobo_duan@126.com>
2014-11-24 16:14:46 +0800

    demo is not mine

---
+++ RACollectionViewReorderableTripletLayout/Example/RACollectionViewReorderableTripletLayout-Demo/RACollectionViewCell.m
@@ -0,0 +1,34 @@
+//
+//  RACollectionViewCell.m
+//  RACollectionViewTripletLayout-Demo
+//
+//  Created by Ryo Aoyama on 5/27/14.
+//  Copyright (c) 2014 Ryo Aoyama. All rights reserved.
+//
+
+#import "RACollectionViewCell.h"
+
+@implementation RACollectionViewCell
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        _imageView = [[UIImageView alloc] init];
+        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+        _imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    }
+    return self;
+}
+
+- (void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    if (highlighted) {
+        _imageView.alpha = .7f;
+    }else {
+        _imageView.alpha = 1.f;
+    }
+}
+
+@end
